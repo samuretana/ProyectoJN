7093f0ff6e655ff6c02fe585be28d26153acac78
samuretana <samuretana2018@gmail.com>
2025-01-30 19:12:36 -0600

    Primer Commit

---
+++ JN_ProyectoWeb/JN_ProyectoWeb/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JN_ProyectoWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+    }
+}

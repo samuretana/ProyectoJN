e19f4b7f5240035f459b9e9c8fe8c7a307952f84
samuretana <samuretana2018@gmail.com>
2025-02-06 16:36:54 -0600

    Fin de Semana 3

---
+++ JN_ProyectoWeb/JN_ProyectoWeb/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JN_ProyectoWeb.Models
+{
+    public class UsuarioModel
+    {
+        public string? Identificacion { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Contrasenna { get; set; }
+    }
+}

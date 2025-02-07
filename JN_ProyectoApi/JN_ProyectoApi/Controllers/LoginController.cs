bc05e0d2071be8d533020a19a2ead349395a92e0
samuretana <samuretana2018@gmail.com>
2025-02-06 21:59:04 -0600

    Semana 4 - Casi terminado

---
+++ JN_ProyectoApi/JN_ProyectoApi/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using JN_ProyectoApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace JN_ProyectoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route ("RegistrarCuenta")]
+        public IActionResult RegistrarCuenta(UsuarioModel model)
+        {
+            using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:BDConnection").Value))
+            {
+
+                var result = context.Execute("RegistrarCuenta",
+                    new { model.Identificacion, model.Nombre, model.Correo, model.Contrasenna });
+
+            }
+
+            return Ok();
+        }
+    }
+}
--- JN_ProyectoWeb/JN_ProyectoWeb/Controllers/LoginController.cs
+++ JN_ProyectoWeb/JN_ProyectoWeb/Controllers/LoginController.cs
@@ -1,23 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JN_ProyectoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JN_ProyectoWeb.Controllers
 {
     public class LoginController : Controller
     {
+
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuratrion;
+        public LoginController(IHttpClientFactory httpClient, IConfiguration configuratrion)
+        {
+            _httpClient = httpClient;
+            _configuratrion = configuratrion;
+        }
+
+        #region RegistrarCuenta
+
+        [HttpGet]
+        public IActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegistrarCuenta(UsuarioModel model)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                string url = _configuratrion.GetSection("Variables:urlWebApi").Value + "Login/RegistrarCuenta";
+                var response = http.PostAsJsonAsync(url, model).Result;
+
+                if (response.IsSuccessStatusCode)
+                    return RedirectToAction("IniciarSesion", "Login");
+                }    
+
+                return View();
+        }
+
+        #endregion
+
+        [HttpGet]
         public IActionResult IniciarSesion()
         {
             return View();
         }
-
+        
+        [HttpGet]
         public IActionResult Principal()
         {
             return View();
         }
 
-        public IActionResult RegistrarCuenta()
-        {
-            return View();
-        }
+      
+
+        [HttpGet]
 
         public IActionResult RecuperarContrasenna()
         {
